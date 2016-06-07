ad4e09bc9706e8cdf00227feffa3541f8dccf0e9
martykells <martykells@gmail.com>
2016-06-07 21:30:09 +0100

    externalised configuration improvements

--- build.ds/src/main/java/com/mxk/buildds/controller/DashboardController.java
+++ build.ds/src/main/java/com/mxk/buildds/controller/DashboardController.java
@@ -14,11 +14,6 @@ public class DashboardController {
 
     @RequestMapping("/")
     public String display(Model model) {
-        System.out.println(jobServerConfig.getType());
-        System.out.println(jobServerConfig.getUrl());
-        System.out.println(jobServerConfig.getUsername());
-        System.out.println(jobServerConfig.getPassword());
-        System.out.println(jobServerConfig.getJoblist());
         return "dashboard";
     }
 
